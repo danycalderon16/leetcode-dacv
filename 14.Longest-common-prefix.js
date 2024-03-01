41b5d905a1df99924dea7453328fe309bcd46518
Daniel Calderon <danycalvir@gmail.com>
2024-02-29 23:25:37 -0700

    longestCommonPrefix fix

--- 14.Longest-common-prefix.js
+++ 14.Longest-common-prefix.js
@@ -2,35 +2,21 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function (strs) {
-  if (strs.length === 0) return "";
-  if (strs.length === 1) return "";
+function longestCommonPrefix(strs) {
+  console.log(strs.sort());
+  console.log(strs);
 
-  const first = strs[0].split("");
-  const second = strs[1].split("");
-  let prefix = getPrefix(first, second);
-
-  if(strs.length === 2) return prefix;
-
-  for (let i = 2; i < strs.length; i++) {
-    if(prefix === "") return []
-    if(strs[i].includes(prefix))
-  }
-  return prefix;
-};
-
-const getPrefix = (first, second) => {
   let prefix = "";
-  for (let i = 0; i < first.length; i++) {
-    if (first[i] === second[i] && second[i] !== undefined) {
-      prefix = prefix + first[i];
-    } else {
-      break;
-    }
-  }
+  const fisrt = strs[0].split("");
+  const last = strs[strs.length - 1].split("");
 
-  return prefix;
-};
+  for (let i = 0; i < fisrt.length; i++) {
+    if (fisrt[i] === undefined || last[i] === undefined) return prefix;
+
+    if (fisrt[i] === last[i]) prefix += fisrt[i];
+    else return prefix;
+  }
+}
 
 console.log(longestCommonPrefix(["flower", "flow", "flight"]));
 // console.log(longestCommonPrefix(["dog","racecar","car"]));
