a3f4b4e7154d757a9bc56d03ba08598de0c76d8e
Daniel Calderon <danycalvir@gmail.com>
2024-03-05 23:23:49 -0700

    Regular expresion matching, solution in python

--- 10.regular-expression-matching.js
+++ 10.regular-expression-matching.js
@@ -3,5 +3,24 @@
  * @param {string} p
  * @return {boolean}
  */
-var isMatch = function (s, p) {};
-console.log(isMatch("aa", "a"));
+var isMatch = function (s, p) {
+  const matchers = [".", "*"];
+  const str = s.split("");
+  const patter = p.split("");
+  const j = 0;
+  for (let i = 0; i < str.length; i++) {
+    const c = str[i];
+    const pa = patter[j];
+    if (c == pa) {
+      j++;
+      continue;
+    }
+    if (pa === ".") {
+      j++;
+      continue;
+    }
+    if (pa === "*") {
+    }
+  }
+};
+console.log(isMatch("aa ", "a"));
