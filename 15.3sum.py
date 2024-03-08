d5c2a427fd22fb81263aec1caead9bdeaee9b66f
Daniel Calderon <danycalvir@gmail.com>
2024-03-07 21:51:39 -0700

    3sum python

--- 15.3sum.py
+++ 15.3sum.py
@@ -4,16 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        nums.sort()
+        
         triplets = set()
-        a = [1,2]
-        b = [2,1]
-        triplets.add(a.sort())
-        triplets.add(b.sort())
-        triplets.add(1)
-        return  triplets
+        output = []
+        for i in range(len(nums)):
+            j = i + 1
+            k = len(nums) - 1 
+            while j < k:
+                sum = nums[i] + nums[j] + nums[k]
+                if sum == 0:
+                    triplet = (nums[i], nums[j], nums[k])
+                    triplets.add(triplet) 
+                    j += 1
+                    k -= 1
+                elif sum < 0:
+                    j = j+1
+                else:
+                    k = k - 1
+
+        output = list(triplet)
+        return  output
 
 
 if __name__ == "__main__":
     solution = Solution()
-    heights = [-1,0,1,2,-1,4]
+    heights = [-1,0,1,2,-1,-4]
     print("Triplets: ", solution.threeSum(heights))
