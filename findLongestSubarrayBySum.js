5a46daddb347cab867a54a2db739f7e2de61beba
Daniel Calderon <danycalvir@gmail.com>
2024-03-10 22:37:35 -0700

    findLongestSubArrayBySum js

--- findLongestSubarrayBySum.js
+++ findLongestSubarrayBySum.js
@@ -1,14 +1,24 @@
 function findLongestSubArray(arr, target) {
-  let left = 1;
-  let rigth = 1;
-  let accum = 0;
+  let result = [-1];
+  let left = 0;
+  let right = 0;
+  let sum = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    accum += arr[i];
-    while (accum <= target) {}
+  while (right < arr.length) {
+    sum += arr[right];
+    while (left < right && sum > target) {
+      sum -= arr[left++];
+    }
+    if (
+      sum === target &&
+      (result.length === 1 || result[1] - result[0] < right - left)
+    ) {
+      result = [left + 1, right + 1];
+    }
+    right++;
   }
 
-  return [left, rigth];
+  return result;
 }
 
 console.log(findLongestSubArray([1, 2, 3, 4, 5, 0, 0, 0, 6, 7, 8, 9, 10], 15));
