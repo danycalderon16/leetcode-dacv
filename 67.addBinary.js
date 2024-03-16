5c470220f0f1463fe16740297c686a2a8e47bc24
Daniel Calderón <34558158+danycalderon16@users.noreply.github.com>
2024-03-16 11:55:24 -0700

    Update 67.addBinary.js

--- 67.addBinary.js
+++ 67.addBinary.js
@@ -3,4 +3,10 @@
  * @param {string} b
  * @return {string}
  */
-var addBinary = function (a, b) {};
+var addBinary = function (a, b) {
+   const aReverse = a.split("").reverse()
+    const bReverse = a.split("").reverse()
+return [aReverse, bReverse]
+}
+
+console.log(addBinary("10011","10101110"))
