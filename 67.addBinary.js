0a4e649fe003e04ff4202ad65be0a3052bf13bf1
Daniel Calderon <danycalvir@gmail.com>
2024-03-17 19:58:18 -0700

    add binary js

--- 67.addBinary.js
+++ 67.addBinary.js
@@ -3,4 +3,34 @@
  * @param {string} b
  * @return {string}
  */
-var addBinary = function (a, b) {};
+var addBinary = function (a, b) {
+  const length = Math.max(a.length, b.length) + 1;
+  const A = a
+    .split("")
+    .reverse()
+    .concat(Array(length - a.length).fill("0"));
+  const B = b
+    .split("")
+    .reverse()
+    .concat(Array(length - b.length).fill("0"));
+
+  const res = Array(length).fill(0);
+
+  for (let i = 0; i < length - 1; i++) {
+    const { cout, s } = sum(parseInt(A[i]), parseInt(B[i]), res[i]);
+    res[i] = s;
+    res[i + 1] = cout;
+  }
+
+  const resum = res.reverse().join("");
+  return resum[0] === "0" ? resum.substring(1) : resum;
+};
+
+function sum(a, b, cin) {
+  return {
+    cout: a * b + cin * (a ^ b),
+    s: a ^ b ^ cin,
+  };
+}
+
+console.log(addBinary("0", "0"));
