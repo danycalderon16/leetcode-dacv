169c0296c167387738e8d9abe559e0ad5a186fd1
Daniel Calderon <danycalvir@gmail.com>
2024-03-18 21:34:14 -0700

    74 search a 2d matrix px O(m+n)

--- 74.seach_a_2d_matrix.py
+++ 74.seach_a_2d_matrix.py
@@ -5,14 +5,41 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
+
+        return self.bsMatrix(matrix,0, len(matrix)-1, target)
     
-    def bs(self,)
+    def bsMatrix(self,matrix, left, right, target):
+        if(left>right):
+            return False;
+        else:
+            piv = (right+left)//2
+            if(target== matrix[piv][0] or target== matrix[piv][-1]):
+                return True
+            else:
+                if (target> matrix[piv][0] and target < matrix[piv][-1]):
+                    return self.bsArray(matrix[piv],0,len(matrix[piv]), target)
+                else:
+                    if(target<matrix[piv][0]):
+                        return self.bsMatrix(matrix,left, piv-1, target)
+                    elif target>matrix[piv][-1]:
+                        return self.bsMatrix(matrix, piv+1, right, target)
+
+    def bsArray(self,arr, left, right, target):
+        if(left>right):
+            return False
+        else:
+            piv = (left + right) //2
+            if(arr[piv] == target):
+                return True
+            else:
+                if target < arr[piv]:
+                    return self.bsArray(arr, left, piv-1, target)
+                else:
+                    return self.bsArray(arr, piv+1, right, target)
+
 
 if __name__ == "__main__":
 
-  sr = Solution()
-
-  res = sr.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3)
-
-  print(res)
-  # 0 1 2 3 4 5 | 6 7 8 | 9 10 |
+    sr = Solution()
+    res = sr.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],22)
+    print(res)
