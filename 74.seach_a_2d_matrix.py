aaaffa013e18f85613ec03d9e5e33207b6169aa6
Daniel Calderon <danycalvir@gmail.com>
2024-03-18 21:10:26 -0700

    74 search a 2d matrix px

---
+++ 74.seach_a_2d_matrix.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+    
+    def bs(self,)
+
+if __name__ == "__main__":
+
+  sr = Solution()
+
+  res = sr.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3)
+
+  print(res)
+  # 0 1 2 3 4 5 | 6 7 8 | 9 10 |
