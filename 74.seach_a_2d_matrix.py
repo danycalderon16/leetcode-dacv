08398af5ed70d068dbd3a81b177d75b99b9bfec6
Daniel Calderon <danycalvir@gmail.com>
2024-03-18 22:13:39 -0700

    74 search a 2d matrix py

--- 74.seach_a_2d_matrix.py
+++ 74.seach_a_2d_matrix.py
@@ -1,45 +1,33 @@
-class Solution(object):
+class Solution:
     def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
+        if not matrix:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
 
-        return self.bsMatrix(matrix,0, len(matrix)-1, target)
-    
-    def bsMatrix(self,matrix, left, right, target):
-        if(left>right):
-            return False;
-        else:
-            piv = (right+left)//2
-            if(target== matrix[piv][0] or target== matrix[piv][-1]):
-                return True
-            else:
-                if (target> matrix[piv][0] and target < matrix[piv][-1]):
-                    return self.bsArray(matrix[piv],0,len(matrix[piv]), target)
-                else:
-                    if(target<matrix[piv][0]):
-                        return self.bsMatrix(matrix,left, piv-1, target)
-                    elif target>matrix[piv][-1]:
-                        return self.bsMatrix(matrix, piv+1, right, target)
+        return self.bs(matrix, left, right, target)
 
-    def bsArray(self,arr, left, right, target):
+    def bs(self, matrix, left, right, target):
         if(left>right):
             return False
         else:
-            piv = (left + right) //2
-            if(arr[piv] == target):
+            mid = (left + right) // 2
+            mid_row, mid_col = divmod(mid,len(matrix[0]))
+
+            if matrix[mid_row][mid_col] == target:
                 return True
             else:
-                if target < arr[piv]:
-                    return self.bsArray(arr, left, piv-1, target)
+                if target < matrix[mid_row][mid_col] :
+                    return self.bs(matrix, left, mid-1, target)
                 else:
-                    return self.bsArray(arr, piv+1, right, target)
+                    return self.bs(matrix, mid+1, right, target)
 
 
 if __name__ == "__main__":
 
     sr = Solution()
-    res = sr.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],22)
+    res = sr.searchMatrix([
+        [1 ,  3,  5,  7],
+        [10, 11, 16, 20],
+        [23, 30, 34, 60]],13)
     print(res)
