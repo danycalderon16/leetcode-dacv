634fc9335b7d8e0780bafcfd7059d18a321efed0
Daniel Calderon <danycalvir@gmail.com>
2024-03-20 22:27:51 -0700

    81.search_rotated_array2 py

--- 81.search_rotated_array2.py
+++ 81.search_rotated_array2.py
@@ -1,44 +1,41 @@
 
 class Solution:
-    def search(self, nums, target: int) -> int:
-      left = 0
-      right = len(nums) - 1
-      return self.bsr(nums, target, left, right)
+	def search(self, nums, target):
+		left = 0
+		right = len(nums) - 1
+		while left <= right:
+			if nums[left] == target or nums[right] == target:
+				return True
 
-    def bsr(self, nums, target:int, left:int, right:int):
-      if(left>right):
-        return -1
-      elif nums[left]==target:
-        return left
-      elif nums[right]==target:
-        return right
-      else:
-        piv = (left+right) // 2
-        middle = nums[piv]
-        if(middle==target):
-          return piv
-        else:
-          # check sorted sublist
-          if nums[left]<middle:#left sorted, right unsorted
-            if target>middle:
-              return self.bsr(nums,target, piv+1, right)
-            else:
-              if target<nums[left]:
-                return self.bsr(nums,target, piv+1, right)
-              else:
-                return self.bsr(nums,target, left, piv-1)
-              
-          else:#left unsorted, right sorted
-            if target<middle:
-              return self.bsr(nums,target, left, piv-1)
-            else:
-              if target<nums[right]:
-                return self.bsr(nums,target, piv+1, right)
-              else:
-                return self.bsr(nums,target, left, piv-1)
+			piv = (left+right)//2
+
+			if nums[left] == nums[piv]:
+				left +=1
+				continue
+
+			if nums[piv] == target:
+				return True
+			
+			if nums[left] < nums[piv] and nums[piv] < nums[right]:#sorted
+				if target < nums[piv]:
+					right -= 1
+				else:
+					left += 1
+			else:#rotated
+				if nums[left] < nums[piv]: #sorted left
+					if target < nums[piv]:
+						right -= 1
+					else:
+						left += 1
+				else:	#sorted right
+					if target > nums[piv]:
+						left += 1
+					else:
+						right -= 1
+		return False
 
 
 if __name__ == "__main__":
-  bs = Solution()
-  print(bs.search([2,5,6,0,0,1,2],3))
-# print(bs.search([0,1,2,3,4,5,6],0))
+    bs = Solution()
+    print(bs.search([0 ,0, 1, 1, 2, 0], 2))
+#   print(bs.search([0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18],0))
