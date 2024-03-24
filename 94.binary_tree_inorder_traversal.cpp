a79ec2abdfdfe79ad6145f6bb1c4e1968d921799
Daniel Calderon <danycalvir@gmail.com>
2024-03-24 12:00:00 -0700

    init 100 same tree

--- 94.binary_tree_inorder_traversal.cpp
+++ 94.binary_tree_inorder_traversal.cpp
@@ -1,22 +1,44 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        
-    }
+#include <iostream>
+#include <vector>
+#include <stack>
 
-    int main(){
-        
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution{
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
     }
 };
 
+
+    int main() {
+         Solution solution;
+
+    // Creating a binary tree
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    // Calling inorderTraversal and printing the result
+    vector<int> inorder = solution.inorderTraversal(root);
+    cout << "Inorder traversal: ";
+    for (int val : inorder) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    // Clean up the memory allocated for the tree nodes
+    // Add cleanup code if necessary
+
+    return 0;
+    }
