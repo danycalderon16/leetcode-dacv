f1cef044d2f733d10e41bda0df4f4993f39c13de
Daniel Calderon <danycalvir@gmail.com>
2024-03-26 23:00:10 -0700

    merge sort py

---
+++ data-structures/merge_sort.py
@@ -0,0 +1,36 @@
+class Solution:
+  def merge_sort(self,list):
+    if len(list) == 1:
+      return list
+    
+    l_left = list[:len(list)//2]
+    l_rigth = list[len(list)//2:]
+
+    l_left = self.merge_sort(l_left)    
+    l_rigth = self.merge_sort(l_rigth)    
+
+    return self.merge(l_left, l_rigth)
+
+  def merge(self, l_left, l_rigth):
+    list = []
+    while len(l_left)>0 and len(l_rigth)>0:
+      if l_left[0] < l_rigth[0]:
+        list.append(l_left[0])
+        l_left = l_left[1:]
+      else:
+        list.append(l_rigth[0])
+        l_rigth = l_rigth[1:]
+
+    if len(l_left) >0:
+      list = list + l_left
+
+    if len(l_rigth) >0:
+      list = list + l_rigth
+    
+    return list
+
+
+if __name__ == "__main__":
+  list = [11, 2, 33, 9, 15]
+  res = Solution().merge_sort(list)
+  print(res)
