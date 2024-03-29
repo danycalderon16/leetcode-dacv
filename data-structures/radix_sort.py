fb9781b5839a42046618dbc919501270db648093
Daniel Calderon <danycalvir@gmail.com>
2024-03-28 22:25:42 -0700

    radix sort py

--- data-structures/radix_sort.py
+++ data-structures/radix_sort.py
@@ -7,6 +7,14 @@ def radix_sort(lista):
   for i in range(0, len(lista)):
     while len(lista[i]) < n:
       lista[i] = "0" + lista[i]
+
+  for j in range(n-1,-1, -1):
+    groups = [ [] for j in range(10)]
+    for i in range(len(lista)):
+      pos = int(lista[i][j])
+      groups[pos].append(lista[i])
+    
+    lista = [ el for group in groups for el in group ]
   
   return lista
 
