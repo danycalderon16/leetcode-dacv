0f4a94b25b03c08fe7197a25a04676dea4f12c4d
Daniel Calderon <danycalvir@gmail.com>
2024-04-16 23:16:35 -0700

    21 merge py

--- 21.merge-two-list.py
+++ 21.merge-two-list.py
@@ -17,6 +17,18 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        cur = dummy = ListNode()
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2, cur = list2.next, list2
+
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
         
 
 if __name__ == "__main__":
