eb62cb4d4240615cca64bb5fbc6b0cb5d8ab309b
Daniel Calderon <danycalvir@gmail.com>
2024-05-02 21:44:29 -0700

    bfs py

--- data-structures/grafos/grafica.py
+++ data-structures/grafos/grafica.py
@@ -24,6 +24,29 @@ class Grafica:
       self.vertices[a].agregaVecino(b)
       self.vertices[b].agregaVecino(a)
 
+  def bfs(self, r):
+    l = []
+    if r in self.vertices:
+      cola = [r]
+      l.append(r)
+      self.vertices[r].visitado = True
+      self.vertices[r].nivel = 0
+
+      print("("+str(r)+","+str(self.vertices[r].nivel)+")")
+
+      while len(cola) > 0:
+        actual = cola[0]
+        cola = cola[1:]
+
+        for v in self.vertices[actual].vecinos:
+          if self.vertices[v].visitado == False:
+            cola.append(v)
+            l.append(v)
+            self.vertices[v].visitado = True
+            self.vertices[v].nivel = self.vertices[actual].nivel + 1
+            print("("+str(v)+","+str(self.vertices[v].nivel)+")" )
+
+    return l
 
 def main():
   g = Grafica()
@@ -32,13 +55,15 @@ def main():
   for v in l:
     g.agregaVertice(v)
 
-  l = [2,0,0,6,6,3,0,5,6,5,0,1,6,4,1,4]
+  l = [1,4,4,3,4,6,3,5,3,2,6,5,5,2]
 
   for i in range(0, len(l)-1, 2):
     g.agregarArista(l[i], l[i+1])
 
-  for v in g.vertices:
-    print(v,g.vertices[v].vecinos)
+  # for v in g.vertices:
+  #   print(v,g.vertices[v].vecinos)
+
+  print(g.bfs(2))
 
 
 if __name__ == '__main__':
