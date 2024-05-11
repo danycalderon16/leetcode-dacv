987ebd547ae1cc278d5c8e081b8d82d2a95d2f07
Daniel Calderon <danycalvir@gmail.com>
2024-05-11 11:57:34 -0700

    lengthOfLastWord js solved

--- 58.LengthOfLastWord.js
+++ 58.LengthOfLastWord.js
@@ -3,16 +3,37 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-  const strs = s.split(" ");
-  const words = [];
-  for (let i = 0; i < strs.length; i++) {
-    const len = strs[i].length;
-    if (len > 0) {
-      words.push(strs[i]);
+  let len = 0;
+  let last = 0;
+  for (let i = 0; i < s.length; i++) {
+    const letter = s.substring(i, i + 1);
+    if (letter !== " ") {
+      len++;
+    } else {
+      if (len !== 0) {
+        last = len;
+        len = 0;
+      }
     }
   }
-
-  return words[words.length - 1].length;
+  if (len !== 0) {
+    last = len;
+  }
+  return last;
+  // return words[words.length - 1].length;
 };
+// var lengthOfLastWord = function (s) {
+//   const strs = s.split(" ");
+//   const words = [];
+//   for (let i = 0; i < strs.length; i++) {
+//     const len = strs[i].length;
+//     if (len > 0) {
+//       words.push(strs[i]);
+//     }
+//   }
 
-console.log(lengthOfLastWord("   fly me   to   the moon  "));
+//   return words[words.length - 1].length;
+// };
+
+// console.log(lengthOfLastWord("   fly me   to   the moon  "));
+console.log(lengthOfLastWord("ld   "));
