2cb4729cb5d9b3d268a883a3eb3b615ab51d94e5
Daniel Calderon <danycalvir@gmail.com>
2024-05-12 12:00:00 -0700

    majority element py

---
+++ 169.majority_element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+if __name__ == '__main__':
+    s = Solution()
+    print(s.majorityElement([3,2,3]))
