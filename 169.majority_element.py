3a926677536c7b7c11f8d6e64182f1dad5df0bb6
Daniel Calderon <danycalvir@gmail.com>
2024-05-13 22:18:45 -0700

    majority element py solved

--- 169.majority_element.py
+++ 169.majority_element.py
@@ -1,9 +1,20 @@
 class Solution(object):
     def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        elements = {}
+
+        for num in nums:
+          if num in elements:
+            elements[num] = elements.get(num) + 1
+          else:  
+            elements[num] = 1
+
+        major = 0
+        for key, value in elements.items():
+          if value > major:
+            major = key
+          
+        return major
+
         
 if __name__ == '__main__':
     s = Solution()
