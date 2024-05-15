cf35d02b76c6dacbf65ed7e352f6b0f480af25c2
Daniel Calderon <danycalvir@gmail.com>
2024-05-14 22:11:01 -0700

    divided two integers

---
+++ 29.divide_two_integers.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        negaviteDivisor = divisor < 0 
+        if negaviteDivisor:
+          divisor *= -1
+        
+        negaviteDivident = dividend < 0
+        if negaviteDivident:
+          dividend *= -1
+
+        quotient = 0
+        
+        while dividend > 0:
+          dividend -= divisor 
+          if dividend < 0:
+            continue
+          quotient += 1
+
+        if negaviteDivident and negaviteDivisor or (not negaviteDivident) and (not negaviteDivisor):
+          return quotient
+
+        if negaviteDivident or negaviteDivisor:
+          return quotient * (-1)
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.divide(7, -3))  
+    print(s.divide(-7, -3))  
+    print(s.divide(7, 3))  
