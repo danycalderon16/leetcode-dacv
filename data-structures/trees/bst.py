101f7b2f28cc102821cab40cce1a5dbab240c8b4
Daniel Calderon <danycalvir@gmail.com>
2025-09-22 22:52:44 -0700

    print tree with inorden, preorden and postorden

--- data-structures/trees/bst.py
+++ data-structures/trees/bst.py
@@ -17,20 +17,45 @@ class BST():
         
         self.insert_rec(self.root,value)
         
-    def insert_rec(self,leef:Node, value:int):
-        if leef.val > value:
-            if not leef.left:
-                leef.left = Node(value)
+    def insert_rec(self,leaf:Node, value:int):
+        if value == leaf.val: return
+        if leaf.val > value:
+            if not leaf.left:
+                leaf.left = Node(value)
             else:
-                self.insert_rec(leef.left, value)
+                self.insert_rec(leaf.left, value)
         else:
-            if not leef.right:
-                leef.right = Node(value)
+            if not leaf.right:
+                leaf.right = Node(value)
             else:
-                self.insert_rec(leef.right, value)
+                self.insert_rec(leaf.right, value)
         
         return
+    def print_tree(self, type="inOrden"):
+        if type == "inOrden":
+            self.in_orden(self.root)
+        if type == "postOrden":
+            self.post_orden(self.root)
+        if type == "preOrden":
+            self.pre_orden(self.root)
         
+    def in_orden(self, root:Node):
+        if not root: return
+        self.in_orden(root.left)
+        print(root.val)
+        self.in_orden(root.right)
+        
+    def post_orden(self, root):
+        if not root: return
+        self.post_orden(root.left)
+        self.post_orden(root.right)
+        print(root.val)
+    
+    def pre_orden(self, root:Node):
+        if not root: return
+        print(root.val)
+        self.pre_orden(root.left)
+        self.pre_orden(root.right)
         
 if __name__ == "__main__":
     tree = BST()
@@ -41,3 +66,4 @@ if __name__ == "__main__":
     tree.insert(1)
     tree.insert(3)
     tree.insert(5)
+    tree.print_tree("preOrden")
