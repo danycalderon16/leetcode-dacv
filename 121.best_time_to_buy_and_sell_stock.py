e17745c6abfe1eaae907b3ae42734b26409436fb
Daniel Calderon <danycalvir@gmail.com>
2025-10-16 22:50:17 -0700

    121 works

--- 121.best_time_to_buy_and_sell_stock.py
+++ 121.best_time_to_buy_and_sell_stock.py
@@ -2,19 +2,13 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        day_to_buy = [1, prices[0]]
-        for i, price in enumerate(prices, start=1):
-            if price < day_to_buy[1]: #position 1
-                day_to_buy[0] = i
-                day_to_buy[1] = price
+        min_price = prices[0]
+        max_profit = 0
 
-        day_to_sell = [day_to_buy[0], day_to_buy[1]]
-        for i, price in enumerate(prices[day_to_buy[0]:], start=day_to_buy[0]):
-            if price > day_to_sell[1]:
-                day_to_sell[0] = i
-                day_to_sell[1] = price
-                
-        return day_to_sell[1] - day_to_buy[1]
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+        return max_profit
     
 if __name__ == "__main__":
     s = Solution()
