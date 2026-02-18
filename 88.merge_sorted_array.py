0b62731c84115201d673a714cc0428cea06f0201
Daniel Calderon <danycalvir@gmail.com>
2026-02-17 22:40:01 -0700

    88 bets 100%

--- 88.merge_sorted_array.py
+++ 88.merge_sorted_array.py
@@ -7,25 +7,30 @@ class Solution:
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        i = m - 1
-        j = n - 1
-        k = m + m - 1
+        piv = m + n - 1
+        m= m-1
+        n = n-1
         
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
+        while True:
+            if n == -1:
+                break
+            elif m == -1:
+                nums1[piv] = nums2[n]
+                n -= 1
+            elif nums1[m] >= nums2[n]:
+                nums1[piv] = nums1[m]
+                m -= 1
             else:
-                nums1[k] = nums2[j]
-                j -= 1 
-            k -= 1
-            
+                nums1[piv] = nums2[n]
+                n -= 1
+            piv -= 1
         return nums1
-        
+    
+    
 if __name__ == "__main__":
     solution = Solution()
-    nums1 = [1,2,3,0,0,0]
-    nums2 = [2,5,6]
-    m = 3
-    n = 3
+    nums1 = [2,0]
+    nums2 = [1]
+    m = 1
+    n = 1
     print(solution.merge(nums1,m,nums2,n))
