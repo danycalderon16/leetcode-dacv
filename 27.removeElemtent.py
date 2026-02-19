6461c6cc41ae1850b78ccdb1699f82a7299ca436
Daniel Calderon <danycalvir@gmail.com>
2026-02-18 22:59:39 -0700

    27 works bus fails some cases

--- 27.removeElemtent.py
+++ 27.removeElemtent.py
@@ -5,10 +5,25 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        i = 0
+        j = len(nums) - 1 
+    
+        while True:
+            if i > j:
+                break
+            if nums[j] == val:
+                nums.pop()
+                j -= 1 
+            if nums[i] == val:
+                nums[i] = nums[j]
+                j -= 1
+                nums.pop()
+            i += 1
 
-        
+        return len(nums)
+
 
 if __name__ == "__main__":
     s = Solution()
-    r = s.removeElement([3,2,2,3],3)
+    r = s.removeElement([3],3)
     print(r)
