c572614cf154486c3d00286c3e979bf6eae9799a
Daniel Calderon <danycalvir@gmail.com>
2026-02-19 22:53:51 -0700

    27 works

--- 27.removeElemtent.py
+++ 27.removeElemtent.py
@@ -1,29 +1,23 @@
 class Solution(object):
     def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
         i = 0
         j = len(nums) - 1 
     
-        while True:
-            if i > j:
-                break
+        while i <= j:
             if nums[j] == val:
                 nums.pop()
                 j -= 1 
-            if nums[i] == val:
+            elif nums[i] == val:
                 nums[i] = nums[j]
                 j -= 1
                 nums.pop()
-            i += 1
+            else:
+                i += 1
 
         return len(nums)
 
 
 if __name__ == "__main__":
     s = Solution()
-    r = s.removeElement([3],3)
+    r = s.removeElement([3,2,2,3],3)
     print(r)
