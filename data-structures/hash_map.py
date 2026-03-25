ee95d2d8e09f386192a370937e2755e90f3d355d
Daniel Calderon <danycalvir@gmail.com>
2026-03-24 21:50:58 -0700

    hashmap, create add and remove methods

--- data-structures/hash_map.py
+++ data-structures/hash_map.py
@@ -1,15 +1,39 @@
-import math
 
 class HashMap:
     
     def __init__(self):
-        self.map = []
+        self.size = 1
+        self.map = [ [] for i in range(0,self.size)]
+    
+    
+    def _hash(self, value):
+        return hash(value) % self.size
     
     def add(self, value):
-        key = math.ceil(value*math.pow(value, 2)*math.pi % 40)
-        print(key)
+        key = self._hash(value)
+        if self.map[key] is []:
+            self.map[key] = [value]
+        else:
+            self.map[key].append(value)
+    
+    def remove(self, value):
+        key = self._hash(value)
+        elements = self.map[key]
+        if elements:
+            for el in elements:
+                if el == value:
+                    elements.remove(el)
+                    return
         
+    def display(self):
+        print(self.map)
 if __name__ == "__main__":
+    
     h = HashMap()
-    for i in range(0,100):
-        h.add(i)    
+    h.add("Hola")
+    h.add("Mundo")
+    h.add("messi")
+    h.add("neymar")
+    h.display()
+    h.remove("neymar")
+    h.display()
